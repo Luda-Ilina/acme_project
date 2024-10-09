35a22471956de16f66bb75102280111321459abf
Liudmila Ilina <liudmilla.ilina@gmail.com>
2024-10-09 14:26:43 +0200

    Learn how to divide the user permissions, verify author

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,5 +1,6 @@
 # birthday/views.py
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import UserPassesTestMixin  
+# LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
@@ -27,6 +28,12 @@ from .utils import calculate_birthday_countdown
 #     form_class = BirthdayForm
 #     template_name = 'birthday/birthday.html'
 
+class OnlyAuthorMixin(UserPassesTestMixin):
+
+    def test_func(self):
+        object = self.get_object()
+        return object.author == self.request.user
+
 
 # Наследуем класс от встроенного ListView:
 class BirthdayListView(ListView):
@@ -53,17 +60,23 @@ class BirthdayListView(ListView):
 #     pass
 
 
-class BirthdayCreateView(LoginRequiredMixin, CreateView):
+class BirthdayCreateView(OnlyAuthorMixin, CreateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+    def form_valid(self, form):
+        # Присвоить полю author объект пользователя из запроса.
+        form.instance.author = self.request.user
+        # Продолжить валидацию, описанную в форме.
+        return super().form_valid(form)
+
+
+class BirthdayUpdateView(OnlyAuthorMixin, UpdateView):
     model = Birthday
     form_class = BirthdayForm
 
 
-class BirthdayUpdateView(LoginRequiredMixin, UpdateView):
-    model = Birthday
-    form_class = BirthdayForm
-
-
-class BirthdayDeleteView(LoginRequiredMixin, DeleteView):
+class BirthdayDeleteView(OnlyAuthorMixin, DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
 
